afd0adaa3f1b29115a6ad61975eaee06492497ee
Grayson A <graysonpa@gmail.com>
2023-10-07 06:44:08 -0700

    init  commit + adaptation to xbox ctrlr + slowMode

---
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -0,0 +1,53 @@
+package frc.robot.commands;
+
+import frc.robot.Constants;
+import frc.robot.subsystems.Swerve;
+
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+
+public class TeleopSwerve extends CommandBase {    
+    private Swerve s_Swerve;    
+    private DoubleSupplier translationSup;
+    private DoubleSupplier strafeSup;
+    private DoubleSupplier rotationSup;
+    private BooleanSupplier slowMode;
+
+    public TeleopSwerve(Swerve s_Swerve, DoubleSupplier translationSup, DoubleSupplier strafeSup, DoubleSupplier rotationSup, BooleanSupplier slowMode) {
+        this.s_Swerve = s_Swerve;
+        addRequirements(s_Swerve);
+
+        this.translationSup = translationSup;
+        this.strafeSup = strafeSup;
+        this.rotationSup = rotationSup;
+        this.slowMode = slowMode;
+    }
+
+    @Override
+    public void execute() {
+        /* Get Values, Deadband*/
+        double translationVal = MathUtil.applyDeadband(translationSup.getAsDouble(), Constants.stickDeadband);
+        double strafeVal = MathUtil.applyDeadband(strafeSup.getAsDouble(), Constants.stickDeadband);
+        double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
+
+        /* Slow down the robot to allow greater precision */
+        if (slowMode.getAsBoolean()) {
+            translationVal *= Constants.Swerve.slowModeSpeedMultiplier;
+            strafeVal *= Constants.Swerve.slowModeSpeedMultiplier;
+            rotationVal *= Constants.Swerve.slowModeTurnMultiplier;
+        }
+
+        /* Drive */
+        s_Swerve.drive(
+            new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed), 
+            rotationVal * Constants.Swerve.maxAngularVelocity, 
+            true, 
+            true
+        );
+    }
+}
