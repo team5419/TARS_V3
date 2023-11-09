b6a64ec2d2380feab0481713c98c3f3d798b53a9
Grayson A <graysonpa@gmail.com>
2023-11-09 14:23:14 -0800

    Probably a better way of doing this

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -67,7 +67,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
-    // vision.updateEntries();
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
--- src/main/java/frc/robot/commands/arm/ArmThrow.java
+++ src/main/java/frc/robot/commands/arm/ArmThrow.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants.ArmTargets;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.arm.OptimizedArm;
@@ -13,7 +14,10 @@ public class ArmThrow extends ParallelCommandGroup {
         addCommands(
             new OptimizedMove(arm, target), // Move
             new SequentialCommandGroup(
-                new WaitCommand(0.2), // Delay the shot
+                // new WaitCommand(0.2), // Delay the shot
+                new WaitUntilCommand(() -> {
+                    return arm.isAt(OptimizedArm.degreesToTicksBicep(80), OptimizedArm.degreesToTicksWrist(90));
+                }),
                 new InstantCommand(() -> intake.set(50)), // Shoot
                 new WaitCommand(0.5), // Wait a sec
                 new InstantCommand(() -> intake.set(0)) // Stop moving
