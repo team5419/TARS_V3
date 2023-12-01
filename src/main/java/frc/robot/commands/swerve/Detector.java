1db4de763b006aa5761ca0b519319e57f17fcc60
Ammelll <ammelsallow@gmail.com>
2023-11-30 18:57:45 -0800

    Updated Detector to Fit new architecture

--- src/main/java/frc/robot/commands/swerve/Detector.java
+++ src/main/java/frc/robot/commands/swerve/Detector.java
@@ -11,49 +11,49 @@ import frc.robot.subsystems.limelight.LimelightHelpers.LimelightResults;
 
 public class Detector {
 
-    private NetworkTable limelight = NetworkTableInstance
+    private NetworkTable pi = NetworkTableInstance
     .getDefault()
-    .getTable("limelight");
-    private NetworkTableEntry tclass;
-    private NetworkTableEntry tx;
-    private NetworkTableEntry ty;
+    .getTable("detector");
+
+    //ASSUMES ONE OBJECT
+    private NetworkTableEntry label;
+    private NetworkTableEntry certainty;
+    private NetworkTableEntry yBottom;
+    private NetworkTableEntry yTop;
+    private NetworkTableEntry xLeft;
+    private NetworkTableEntry xRight;
+
     private ShuffleboardTab tab = Shuffleboard.getTab("detector");
     private boolean alive = true;
     private Notifier limelightStatusNotifier = new Notifier(() -> {
-    LimelightResults current = LimelightHelpers.getLatestResults("limelight");
-    alive = current != null;
+    //!! Alter to reflect new architecture !!// 
+    // LimelightResults current = LimelightHelpers.getLatestResults("limelight");
+    // alive = current != null;
     });
 
     public Detector(){
-        limelightStatusNotifier.startPeriodic(0.25);
-        configLL();
+        // limelightStatusNotifier.startPeriodic(0.25);
         configTab();
     }
     private void configTab(){
-        tab.addString("Class: ", () -> tclass.getString("NULL"));
-        tab.addDouble("TX: ", () -> tx.getDouble(0.0));
-        tab.addDouble("TY: ", () -> ty.getDouble(0.0));
-    }
-    private void configLL(){
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(3);
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(0);
+        tab.addString("Label: ", () -> label.getString("NULL"));
+        tab.addDouble("Y BOTTOM: ", () -> yBottom.getDouble(0.0));
+        tab.addDouble("Y TOP: ", () -> yTop.getDouble(0.0));
+        tab.addDouble("X LEFT: ", () -> xLeft.getDouble(0.0));
+        tab.addDouble("X RIGHT: ", () -> xRight.getDouble(0.0));
+        tab.addDouble("CERTAINTY", certainty.getDouble(0.0));`
     }
 
     public void updateEntries(){
-        tclass = limelight.getEntry("tclass");
-        tx = limelight.getEntry("tx");
-        ty = limelight.getEntry("ty");
+        label = pi.getEntry("Label");
+        certainty = pi.getEntry("Certainty");
+        yBottom = pi.getEntry("Y Bottom");
+        yTop = pi.getEntry("Y Top");
+        xLeft = pi.getEntry("X Left");
+        xRight = pi.getEntry("X Right");
+
     }
     public boolean isAlive() {
         return alive;
-    }
-    public String getTClass(){
-        return tclass.getString("NULL");
-    }
-    public double getTX(){
-        return tx.getDouble(0.0);
-    }
-    public double getTY(){
-        return ty.getDouble(0.0);
     }
 }
